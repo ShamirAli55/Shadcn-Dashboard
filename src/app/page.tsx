0f4fd6d032c1896e51a7becbece01d9c4506cdfe
ShamirAli55 <shamirali9779@gmail.com>
2025-09-12 11:36:14 +0500

    Recharts added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,21 @@
+import AppBarChart from "@/components/AppBarChart"
+import AppAreaChart from "@/components/AppAreaChart"
+import { AppPieChart } from "@/components/AppPieChart"
 const Homepage = () => {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
-        <div className="bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
-        <div className="bg-primary-foreground rounded-lg p-4">Test</div>
-        <div className="bg-primary-foreground rounded-lg p-4">Test</div>
-        <div className="bg-primary-foreground rounded-lg p-4">Test</div>
-        <div className="bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">Test</div>
-        <div className="bg-primary-foreground rounded-lg p-4">Test</div>
+      <div className="bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppBarChart />
+      </div>
+      <div className="bg-primary-foreground rounded-lg p-4">Test</div>
+      <div className="bg-primary-foreground rounded-lg p-4">
+        <AppPieChart />
+      </div>
+      <div className="bg-primary-foreground rounded-lg p-4">Test</div>
+      <div className="bg-primary-foreground rounded-lg p-4 lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+        <AppAreaChart />
+      </div>
+      <div className="bg-primary-foreground rounded-lg p-4">Test</div>
     </div>
   )
 }
